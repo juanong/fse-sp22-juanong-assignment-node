ba32ba960d7566df7601f2582401893af3d64069
Juan Ong <juanong26@gmail.com>
2022-03-17 16:07:13 -0400

    implemented express session as well as signup, profile, logout, and login functionality

--- server.ts
+++ server.ts
@@ -21,13 +21,33 @@ import LikeController from "./controllers/LikeController";
 import BookmarkController from "./controllers/BookmarkController";
 import FollowController from "./controllers/FollowController";
 import MessageController from "./controllers/MessageController";
+import AuthenticationController from "./controllers/AuthenticationController";
 import mongoose from 'mongoose';
 import 'dotenv/config'
 
+const session = require("express-session");
 const app = express();
+let sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        secure: false
+    }
+}
+
+if (process.env.ENV === 'production') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true;
+}
+
+
 var cors = require('cors');
+app.use(session(sess));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    // delete this?
+    origin: 'http://localhost:3000'
+}));
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
 
@@ -49,6 +69,7 @@ const likeController = LikeController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 const followController = FollowController.getInstance(app);
 const messageController = MessageController.getInstance(app);
+AuthenticationController(app);
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku if available.

32b69f45280bfdb8662cb04550b44ab073376179
Juan Ong <juanong26@gmail.com>
2022-03-22 21:34:48 -0400

    configure cors headers

--- server.ts
+++ server.ts
@@ -49,6 +49,17 @@ app.use(cors({
     // add netlify url?
     origin: 'http://localhost:3000'
 }));
+
+// Trying this snippet to prevent CORS issues when using Heroku
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', 'true')
+    next();
+})
+
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
 
